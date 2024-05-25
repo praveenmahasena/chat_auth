7c95dac19765864fd32c1d54e68e5519f09909a5
praveenmahasena <praveenmahasena647@gmail.com>
2024-05-25 11:40:20 -0400

    feature: adding login func

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/praveenmahasena647/chat-app/internal/helpers"
-	"github.com/praveenmahasena647/chat-app/internal/postgres"
 	"github.com/praveenmahasena647/chat-app/internal/server/handlers"
 )
 
@@ -31,28 +30,7 @@ func (s *Server) Run() error {
 
 	g.GET("/mark/:id", handlers.MarkVerify)
 
-	g.POST("/login", func(ctx *gin.Context) {
-		u := postgres.NewLoginCredentials()
-		if err := ctx.Bind(u); err != nil {
-			ctx.JSONP(http.StatusMethodNotAllowed, "the credentials are not allowed")
-			return
-		}
-		usrInfo, usrInfoErr := u.Login(ctx)
-		if usrInfoErr != nil {
-			ctx.JSONP(http.StatusNotFound, usrInfoErr)
-			return
-		}
-		if err := helpers.VerifyPassword(usrInfo.Password, u.Password); err != nil {
-			ctx.JSONP(http.StatusNotFound, err)
-			return
-		}
-		JWT, JWTErr := helpers.GenerateJWT(u.EmailID)
-		if JWTErr != nil {
-			ctx.JSONP(http.StatusNotFound, JWTErr)
-			return
-		}
-		ctx.JSONP(http.StatusOK, JWT)
-	})
+	g.POST("/login", handlers.Login)
 
 	g.GET("/chatroom", handlers.RouteGurd, func(ctx *gin.Context) {
 		ctx.Status(http.StatusOK)
