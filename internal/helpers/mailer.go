800b277225bb582139763ea91c871860991ee638
praveenmahasena <praveenmahasena647@gmail.com>
2024-05-26 12:44:26 -0400

    removed shared map

--- internal/helpers/mailer.go
+++ internal/helpers/mailer.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"errors"
 	"fmt"
 	"net/smtp"
 	"sync"
@@ -9,31 +8,22 @@ import (
 	"github.com/spf13/viper"
 )
 
-var (
-	c = map[string]string{
-		"URL":       "",
-		"FROM":      "",
-		"EPASSWORD": "",
-	}
-)
+const urlParam = "URL"
+const fromParam = "FROM"
+const ePasswordParam = "EPASSWORD"
 
 func Mailer(JWT, mailID string, wg *sync.WaitGroup, ch chan<- error) {
 	defer wg.Done()
 	defer close(ch)
 
-	for e := range c {
-		val := viper.GetString(e)
-		if val == "" {
-			ch <- errors.New("error during reading config file")
-			return
-		}
-		c[e] = val
-	}
+	authUrl := viper.GetString(urlParam)
+	from := viper.GetString(fromParam)
+	password := viper.GetString(ePasswordParam)
 
 	to := []string{mailID}
-	smtpHost, smtpPort := "smtp.gmail.com", "587"
+	smtpHost, smtpPort := "smtp.gmail.com", "587" // TODO: change these and get them into .env
 
-	auth := smtp.PlainAuth("", c["FROM"], c["EPASSWORD"], smtpHost)
+	auth := smtp.PlainAuth("", from, password, smtpHost)
 
 	M := fmt.Sprintf("To: %v\r\n"+
 
@@ -41,9 +31,9 @@ func Mailer(JWT, mailID string, wg *sync.WaitGroup, ch chan<- error) {
 
 		"\r\n"+
 
-		"%v\r\n", c["FROM"], c["URL"]+JWT)
+		"%v\r\n", from, authUrl+JWT)
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, c["FROM"], to, []byte(M))
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(M))
 
 	if err != nil {
 		ch <- err
