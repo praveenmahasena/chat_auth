5aeea282cbfb52bcb534291d3a33e5e5b0e6c5b6
praveenmahasena <praveenmahasena647@gmail.com>
2024-06-02 03:24:30 -0400

    DB_roles: creating psql user roles

--- schema/up/001.create_psql_admin_user_roles.up.sql
+++ schema/up/001.create_psql_admin_user_roles.up.sql
@@ -1,3 +1,5 @@
 CREATE USER read_only WITH PASSWORD 'read_only_dev';
+
 CREATE USER read_write WITH PASSWORD 'read_write_dev';
+
 CREATE USER super_user WITH PASSWORD 'super_user';
---
+++ schema/up/001.grant_privileges.up.sql
@@ -0,0 +1,5 @@
+GRANT ALL ON chat_app TO super_user;
+
+GRANT READ,WRITE ON chat_app TO read_write;
+
+GRANT READ ON chat_app TO read_only;
